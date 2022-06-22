9580ebd3ea321c2b561d337b2ada778e92422015
Pierre Rineau <pierre.rineau@makina-corpus.com>
2022-06-22 09:40:18 +0200

    add legacy makinacorpus/goat event info extractor

---
+++ src/Bridge/EventStore/LegacyAggregateEventInfoExtrator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare (strict_types=1);
+
+namespace MakinaCorpus\CoreBus\Bridge\EventStore;
+
+use MakinaCorpus\Message\BackwardCompat\AggregateMessage;
+
+/**
+ * Supports makinacorpus/goat interface.
+ */
+final class LegacyAggregateEventInfoExtrator implements EventInfoExtrator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function extract(object $event, EventInfo $eventInfo): void
+    {
+        if ($event instanceof AggregateMessage) {
+            if ($aggregateType = $event->getAggregateType()) {
+                $eventInfo->withAggregateType($aggregateType);
+            }
+            if ($aggregateId = $event->getAggregateId()) {
+                $eventInfo->withAggregateId($aggregateId);
+            }
+            if ($aggregateRoot = $event->getAggregateRoot()) {
+                $eventInfo->withAggregateRoot($aggregateRoot);
+            }
+        }
+    }
+}
