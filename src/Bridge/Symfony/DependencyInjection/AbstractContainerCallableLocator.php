2abe7eb7d7c5f86edc0eefd7bc0237902ee4dc68
Pierre Rineau <pierre.rineau@makina-corpus.com>
2023-05-01 15:29:08 +0200

    changes service locator dependency to psr container interface instead

--- src/Bridge/Symfony/DependencyInjection/AbstractContainerCallableLocator.php
+++ src/Bridge/Symfony/DependencyInjection/AbstractContainerCallableLocator.php
@@ -10,28 +10,28 @@ use MakinaCorpus\ArgumentResolver\Context\ArrayResolverContext;
 use MakinaCorpus\CoreBus\Cache\CallableReference;
 use MakinaCorpus\CoreBus\Cache\CallableReferenceList;
 use MakinaCorpus\CoreBus\Cache\RuntimeCallableReferenceList;
-use Symfony\Component\DependencyInjection\ServiceLocator;
+use Psr\Container\ContainerInterface;
 
 abstract class AbstractContainerCallableLocator
 {
     protected CallableReferenceList $referenceList;
     private ?ArgumentResolver $argumentResolver = null;
-    private ?ServiceLocator $serviceLocator = null;
+    private ?ContainerInterface $container = null;
 
     /**
      * @param array<string,string>|CallableReferenceList $references
      */
-    public function __construct($references, ?ServiceLocator $serviceLocator = null, ?ArgumentResolver $argumentResolver = null)
+    public function __construct($references, ?ContainerInterface $container = null, ?ArgumentResolver $argumentResolver = null)
     {
         $this->argumentResolver = $argumentResolver ?? new DefaultArgumentResolver();
-        $this->serviceLocator = $serviceLocator;
+        $this->container = $container;
 
         if ($references instanceof CallableReferenceList) {
             $this->referenceList = $references;
         } else if (\is_array($references)) {
             $this->referenceList = new RuntimeCallableReferenceList($this->allowMultiple());
             foreach ($references as $id => $className) {
-                $this->referenceList->appendFromClass($className, $id);
+                $this->referenceList->appendFromClass($className, \is_int($id) ? $className : $id);
             }
         }
     }
@@ -40,12 +40,12 @@ abstract class AbstractContainerCallableLocator
 
     protected function createCallable(CallableReference $reference): callable
     {
-        if (!$this->serviceLocator) {
+        if (!$this->container) {
             throw new \LogicException("Misinitialized event listener locator.");
         }
 
         $callable = \Closure::fromCallable([
-            $this->serviceLocator->get($reference->serviceId),
+            $this->container->get($reference->serviceId),
             $reference->methodName
         ]);
 
