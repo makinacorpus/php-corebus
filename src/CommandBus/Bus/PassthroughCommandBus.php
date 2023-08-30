ad6ba20c7253604b9a082d211e16aa91e37325f2
Pierre Rineau <pierre.rineau@makina-corpus.com>
2023-08-30 17:48:32 +0200

    passthrough command bus was not passing an envelope

--- src/CommandBus/Bus/PassthroughCommandBus.php
+++ src/CommandBus/Bus/PassthroughCommandBus.php
@@ -7,6 +7,7 @@ namespace MakinaCorpus\CoreBus\CommandBus\Bus;
 use MakinaCorpus\CoreBus\CommandBus\CommandConsumer;
 use MakinaCorpus\CoreBus\CommandBus\CommandResponsePromise;
 use MakinaCorpus\CoreBus\CommandBus\SynchronousCommandBus;
+use MakinaCorpus\Message\Envelope;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
@@ -28,8 +29,14 @@ final class PassthroughCommandBus extends AbstractCommandBus implements Synchron
      */
     public function dispatchCommand(object $command, ?array $properties = null): CommandResponsePromise
     {
-        $this->logger->debug("PassthroughCommandBus: Passing command to consumer: {command}, dropping", ['command' => $command]);
+        $this->logger->debug("PassthroughCommandBus: Passing command to consumer: {command}", ['command' => $command]);
 
-        return $this->commandConsumer->consumeCommand($command);
+        return $this->commandConsumer->consumeCommand(
+            // Wrap properties ensures that all properties added by decorators
+            // end up in the envelope and allows command consumers to read them.
+            // Information such as the user_id when plugged into Symfony can be
+            // critical to have.
+            Envelope::wrap($command, $properties ?? [])
+        );
     }
 }
